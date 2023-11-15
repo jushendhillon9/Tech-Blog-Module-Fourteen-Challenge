0177822a515dbcee1ba204e8c4abcc3641cb7b4a
jushen dhillon <jushendhillon@London-Macbook.local>
2023-11-14 20:48:18 -0800

    adding jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
-//create sequelize connection
-const Sequelize = require("sequelize"); //requiring the sequelize class from the sequelize library
-require("dotenv").config();
-
-const sequelize = new Sequelize( //creating an instance of the sequelize class using new keyword
-//this is what allows sequelize to connect to a mysql database
+const Sequelize = require('sequelize');
+require('dotenv').config();
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
     }
-)
-
+  );
+}
 module.exports = sequelize;
